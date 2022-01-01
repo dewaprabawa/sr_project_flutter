de269c999eb69e648aaedd4a8983a1c0fd8d5819
osls <balipastika@gmail.com>
2022-01-01 16:51:11 +0800

    clear code

--- lib/app/features/auth/data/datasource/remote_user_data_source.dart
+++ lib/app/features/auth/data/datasource/remote_user_data_source.dart
@@ -7,13 +7,12 @@ import 'package:sr_project_flutter/app/shared/exception/exception.dart';
 import 'package:sr_project_flutter/app/shared/network/nt_request.dart';
 
 abstract class RemoteUserDataSource {
-   Future<AppUser> registerUser(AppUser user);
-   Future<AppUser> login(String username, String password);
-   Future<AppUser> getUser();
+  Future<AppUser> registerUser(AppUser user);
+  Future<AppUser> login(String username, String password);
+  Future<AppUser> getUser();
 }
 
 class RemoteUserDataSourceImpl implements RemoteUserDataSource {
-  
   final http.Client client;
 
   const RemoteUserDataSourceImpl(this.client);
@@ -21,55 +20,50 @@ class RemoteUserDataSourceImpl implements RemoteUserDataSource {
   @override
   Future<AppUser> login(String username, String password) async {
     final _url = Uri.parse("http://127.0.0.1:8080/api/auth/signIn");
-    try{
-      final _body = jsonEncode({"username":username, "password": password});
+    try {
+      final _body = jsonEncode({"username": username, "password": password});
       final response = await client.post(_url, body: _body);
-      if(response.statusCode != 200){
+      if (response.statusCode != 200) {
         final message = jsonDecode(response.body)["message"];
         throw HttpRequestException(message: message);
       }
       final decodedResponse = jsonDecode(response.body);
       return AppUser.fromJson(decodedResponse);
-    }catch(e){
-     throw RemoteServerException(message: e.toString());
+    } catch (e) {
+      throw RemoteServerException(message: e.toString());
     }
   }
 
   @override
   Future<AppUser> registerUser(AppUser user) async {
-       final _url = Uri.parse("http://127.0.0.1:8080/api/auth/signUp");
-    try{
+    final _url = Uri.parse("http://127.0.0.1:8080/api/auth/signUp");
+    try {
       final _body = jsonEncode(user.toJson());
       final response = await client.post(_url, body: _body);
-      if(response.statusCode != 200){
+      if (response.statusCode != 200) {
         final message = jsonDecode(response.body)["message"];
         throw HttpRequestException(message: message);
       }
       final decodedResponse = jsonDecode(response.body);
       return AppUser.fromJson(decodedResponse);
-    }catch(e){
-     throw RemoteServerException(message: e.toString());
+    } catch (e) {
+      throw RemoteServerException(message: e.toString());
     }
   }
 
   @override
   Future<AppUser> getUser() async {
-         final _url = Uri.parse("http://127.0.0.1:8080/api/user");
-    try{
+    final _url = Uri.parse("http://127.0.0.1:8080/api/user");
+    try {
       final response = await client.get(_url);
-      if(response.statusCode != 200){
+      if (response.statusCode != 200) {
         final message = jsonDecode(response.body)["message"];
         throw HttpRequestException(message: message);
       }
       final decodedResponse = jsonDecode(response.body);
       return AppUser.fromJson(decodedResponse);
-    }catch(e){
-     throw RemoteServerException(message: e.toString());
+    } catch (e) {
+      throw RemoteServerException(message: e.toString());
     }
   }
-
 }
-
-
-
-
